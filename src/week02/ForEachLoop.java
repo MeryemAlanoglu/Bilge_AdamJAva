6161ad1afc2ca7301a4d170108ea2318d0551abd
bilge.adam <meryemalanoglu7@gmail.com>
2024-03-22 17:15:55 +0300

    Git v

---
+++ src/week02/ForEachLoop.java
@@ -0,0 +1,16 @@
+package week02;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+        //declaration,intialization
+        int arr[]={12,23,44,56,78};//alt+8,alt+9
+        for (int i:arr){
+            int sum=i+10;
+            if(i<50){
+                System.out.println("50 den küçük");
+                {else}
+                    System.out.println("50 den büyük");
+        }
+        System.out.println("+10 eklenmiş hali:");
+                }
+                }
---
+++ src/week02/soru1.java
@@ -0,0 +1,15 @@
+package week02;
+
+import java.util.Scanner;
+
+public class soru1 {
+    public static void main(String[] args) {
+        //birden başlayarak kullanıcının dışarıdan girdiği kadar olan sayıların
+        // bir fazlasının toplamını ekrana yazdıran algoritma.
+
+        Scanner scanner= new Scanner(System.in);
+        int sayi= scanner.nextInt();
+
+
+    }
+}
